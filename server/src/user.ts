4d9178073e2584e5d42a8d90ad759242f0844fcf
Tom <tomp@Mac-303.lan>
2026-01-08 09:54:18 -0800

    Added User functions for database

--- server/src/user.ts
+++ server/src/user.ts
@@ -27,22 +27,46 @@ export class AuthService {
     )
   }
 
-  private hashPassword = async (password: string): Promise<string> {
+  private hashPassword = async (password: string): Promise<string> => {
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
     return passwordHash;
   }
 
   private userLookup = async (name: string): Promise<User | null> => {
-    const user = await pool.query("SELECT uuid,name,passwordHash FROM users WHERE name = ?", [name]) as User[];
-    if (user[0] != undefined) {
-      return user[0];
-    }
-    return null;
+    let conn;
+
+    try {
+      conn = await pool.getConnection(); 
+      const user = await conn.query("SELECT uuid,name,passwordHash FROM users WHERE name = ?", [name]) as User[];
+      return user[0] || null;
+
+    } catch (err) {
+      console.log(err);
+      return null;
+
+    } finally {
+      if (conn) conn.release();
+    } 
   }
 
   private registerUser = async (name: string, passwordHash: string): Promise<User | null> => {
-    
+    let conn;
+    const uuid = uuidv4();
+    try {
+
+      conn = await pool.getConnection();
+      const result = await conn.query("INSERT INTO users (uuid, name, passwordHash) VALUES (?,?,?)", [uuid, name, passwordHash]);
+      const newUser = await this.userLookup(name);
+      return newUser;
+
+    } catch (err) {
+      console.log(err);
+      return null;
+
+    } finally {
+      if (conn) conn.release();
+    }
   }
 
   loginUser = async (name: string, password: string): Promise<User | null> => {
@@ -50,7 +74,7 @@ export class AuthService {
     const passwordHash = await this.hashPassword(password);
 
     if (!user) {
-      console.log(`User {name} didn't exist, creating...`);
+      console.log(`User '{name}' didn't exist, creating new entry.`);
       const newUser = await this.registerUser(name, passwordHash);
       return newUser; 
     }
